73b86d94589937390744f66bdea551bde7a6c591
edyaca <39631502+edyaca@users.noreply.github.com>
2023-12-20 15:23:23 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { isEmpty } from 'lodash'
+import { isEmpty, size } from 'lodash'
 import { nanoid } from 'nanoid'
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
   const [tasks, setTasks] = useState([])
 
   const addTask = (e) => {
+    console.log(e)
     e.preventDefault()
     if (isEmpty(task)) {
       console.log('Task empty')
@@ -16,10 +17,15 @@ function App() {
     const newTask = {
       id: nanoid(),
       name: task
-    } 
+    }
 
     setTasks([...tasks, newTask])
     setTask('')
+  }
+
+  const deleteTask = (id) => {
+    const filteredTask = tasks.filter(task => task.id !== id)
+    setTasks(filteredTask)
   }
   return (
     <div className='container mt-5'>
@@ -28,20 +34,37 @@ function App() {
       <div className='row'>
         <div className='col-8'>
           <h4 className='text-center'>Lista de Tareas</h4>
-          <ul className='list-group'>
-            {
-              tasks.map((task) => (
-                <li className='list-group-item' key={task.id}>
-                  <span className='lead'>{task.name}</span>
-                  <button className='btn btn-danger btn-sm float-right mx-2'>Eliminar</button>
-                  <button className='btn btn-warning btn-sm float-right'>Editar</button>
-                </li>
-              ))
-            }
-          </ul>
+          {
+            size(tasks) === 0 ? (
+              <h5 className='text-center'>Aun no hay tareas programadas.</h5>
+            ) : (
+              <ul className='list-group'>
+                {
+                  tasks.map((task) => (
+                    <li className='list-group-item' key={task.id}>
+                      {console.log(task)}
+                      {console.log(tasks)}
+                      <span className='lead'>{task.name}</span>
+                      <button
+                        className='btn btn-danger btn-sm float-right mx-2'
+                        onClick={() => deleteTask(task.id)}
+                      >
+                        Eliminar
+                      </button>
+                      <button
+                        className='btn btn-warning btn-sm float-right'
+                      >
+                        Editar
+                      </button>
+                    </li>
+                  ))
+                }
+              </ul>
+            )
+          }
         </div>
         <div className='4'>
-          <h4 className='text-center'>Formularios</h4>
+          <h4 className='text-center'>Agregar Tareas</h4>
           <form onSubmit={addTask}>
             <input
               type='text'
@@ -64,3 +87,6 @@ function App() {
 }
 
 export default App
+
+
+
